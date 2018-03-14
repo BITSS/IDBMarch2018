fb49070d156e9e7b942ed655e9b18fb2a7782a09
Garret Christensen <garret@berkeley.edu>
2018-03-14 16:35:41 -0700

    false pos plot, software links

---
+++ 4-MultipleTesting/plot.R
@@ -0,0 +1,9 @@
+#Plot the rate of at least one false positives
+setEPS()
+postscript("falsepositive.eps")
+curve(1-(1-.05)^x, 0, 100, xlab="Number of tests", ylab="Rate")
+title(main="Rate of at least one false positive by number of tests")
+mtext(expression(paste(alpha,"=0.05")))
+dev.off()
+
+
